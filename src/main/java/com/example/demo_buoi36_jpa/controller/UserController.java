d5fba43345ed78036affe78559abc39af56a9ace
hoa-nguyen-bk <haveaniceday6174@gmail.com>
2025-03-02 09:31:44 +0700

    Implement user retrieval in UserController and update endpoint response

--- src/main/java/com/example/demo_buoi36_jpa/controller/UserController.java
+++ src/main/java/com/example/demo_buoi36_jpa/controller/UserController.java
@@ -1,14 +1,26 @@
 package com.example.demo_buoi36_jpa.controller;
 
+import com.example.demo_buoi36_jpa.entity.Users;
+import com.example.demo_buoi36_jpa.repository.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
+    private UserRepository usersRepository;
+
     @GetMapping
     public ResponseEntity<?> getUser(){
-        return new ResponseEntity<>("", HttpStatus.OK);
+        List<Users> listUser = usersRepository.findAll();
+        return ResponseEntity.ok(listUser);
     }
 }
